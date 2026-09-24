55c0b38f4f15a0b9ff34a318640c4cfe597d44f2
PratikNaskar11 <pratiknaskar11@gmail.com>
2026-09-25 02:20:01 +0530

    Initialized for render

--- embees-whatsapp-marketting/routes/contacts.js
+++ embees-whatsapp-marketting/routes/contacts.js
@@ -1,16 +1,16 @@
-
 import express from "express";
 import db from "../db.js";
 
 const router = express.Router();
 
-router.get("/", async(req,res)=>{
-    try{
-        const [contacts] = await db.query("SELECT * FROM contacts");
-        res.json(contacts);
-    }catch(error){
+router.get("/", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM contacts");
+
+        res.json(result.rows);
+    } catch (error) {
         console.error(error);
-        res.status(500).json({message:"Server Error" });
+        res.status(500).json({ message: "Server Error" });
     }
 });
 
